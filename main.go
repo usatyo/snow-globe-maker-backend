0b74dd2a26d94b28abc52317885ebbc4bf1ec37f
karintou8710 <karintou8710@gmail.com>
2023-02-13 02:32:35 +0900

    [refactor] delete debug code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -61,8 +60,6 @@ func main() {
 				Items:        nil,
 			})
 		}
-
-		fmt.Println(searchParams)
 
 		resultCities := searchCityModel(searchParams)
 
